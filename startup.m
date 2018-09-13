23ca3ec32643b1264cd5131982969643002f8afa
JuanPi Carbajal <ajuanpi+dev@gmail.com>
2018-09-13 02:50:55 +0200

    doc: document startup script

--- startup.m
+++ startup.m
@@ -3,10 +3,19 @@
 % Copyright (c) by Carl Edward Rasmussen and Hannes Nickisch 2018-08-01.
 
 disp ('executing gpml startup script...')
-mydir = fileparts (mfilename ('fullpath'));                 % where am I located
-addpath (mydir)
-dirs = {'cov','doc','inf','lik','mean','prior','util'};           % core folders
-for d = dirs, addpath (fullfile (mydir, d{1})), end
-dirs = {{'util','minfunc'},{'util','minfunc','compiled'}};     % minfunc folders
-for d = dirs, addpath (fullfile (mydir, d{1}{:})), end
+mydir = fileparts (mfilename ('fullpath'));        % where am I located
+addpath (mydir);
+
+% core folders
+dirs = {'cov','doc','inf','lik','mean','prior','util'};
+for d = dirs
+  addpath (fullfile (mydir, d{1}))
+end
+
+% minfunc folders
+dirs = {{'util','minfunc'},{'util','minfunc','compiled'}};
+for d = dirs
+  addpath (fullfile (mydir, d{1}{:}))
+end
+
 addpath([mydir,'/util/sparseinv'])
